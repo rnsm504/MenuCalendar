3cd60fd0d7254051ba2dac2e3a913119e07c155b
Masanori Kuze <masanori@masanori-no-imac.local>
2017-08-14 10:43:18 +0900

    Initial Commit

---
+++ MenuCalendar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuCalendar
+//
+//  Created by Masanori Kuze on 2017/08/14.
+//  Copyright © 2017年 Masanori Kuze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuCalendar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuCalendar
+//
+//  Created by Masanori Kuze on 2017/08/14.
+//  Copyright © 2017年 Masanori Kuze. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
